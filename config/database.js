46f40ef5cb83afc93008a9ed3badeb51649c3dc6
kiet0807 <kiet080797@gmail.com>
2021-10-22 01:44:27 +0700

    add

--- config/database.js
+++ config/database.js
@@ -1,39 +1,20 @@
-const parse = require("pg-connection-string").parse;
-
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connections: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          username: config.user,
-          password: config.password,
-        },
-        options: {
-          ssl: false,
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        host: env("DATABASE_HOST", "cluster0.xuem5.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "bakery-app-database"),
+        username: env("DATABASE_USERNAME", "kiet0807"),
+        password: env("DATABASE_PASSWORD", "kiet0347551122"),
       },
-    };
-  }
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
-  };
-};
+  },
+});
